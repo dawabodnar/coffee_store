ac267e837eb2eadf312c2e7ac994d4fb5e4db905
Illia Trach <illia.trach@ihsmarkit.com>
2021-03-18 20:08:32 +0200

    example js

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
 console.log(BEAN_PRODUCTS);
+
+
+console.log("<div> <img src='https://nakavu.com.ua/wp-content/uploads/barista-amato-250g-zerno-1.jpg' class='image'><div>" + BEAN_PRODUCTS[0].name + "</div></div>")

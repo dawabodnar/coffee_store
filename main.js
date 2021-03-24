441fe9bdfeefa0928367725258270c55cfd65a55
Illia Trach <illia.trach@ihsmarkit.com>
2021-03-24 20:21:14 +0200

    Додав поле id для корзини

--- main.js
+++ main.js
@@ -15,9 +15,12 @@ for( var i = 0; i < BEAN_PRODUCTS.length; i++){
         + "<img src='" + BEAN_PRODUCTS[i]. bigPhotoUrl + "' class='image'>" 
         + "<div>" + BEAN_PRODUCTS[i].name + "</div>" 
         + "<div>" + BEAN_PRODUCTS[i].price + "</div>"   
-        + "<button class='button'>В корзину</button>"
+        + "<button onClick='addToCart(" + BEAN_PRODUCTS[i].id + ")' class='button'>В корзину</button>"
     + "</div>"
 
 }
 
 
+function addToCart(id) {
+    console.log(id);
+}

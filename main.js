037b05e7168258fc61ed181ef88ae060a999ae22
Dasha Dodnar <dawa.bodnar@gmail.com>
2021-03-30 13:19:24 +0300

    added coffee in cart

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-
-var arrayCart ={};
-
-// var products = document.getElementById("products-list-id");
-// products.innerHTML = "<div>" 
-//     + "<img src='https://nakavu.com.ua/wp-content/uploads/barista-amato-250g-zerno-1.jpg' class='image'>" 
-//     + "<div>" + BEAN_PRODUCTS[0].name + "</div>" 
-//     + "<div>" + BEAN_PRODUCTS[0].price + "</div>" 
-//     + "<button class='button'>В корзину</button>"
-// + "</div>"
-
+var saveInCart = localStorage.getItem('cartstorage')
+var cartStorage;
+if (saveInCart === null) {
+    cartStorage = {}
+}
+else {
+    cartStorage = JSON.parse(saveInCart);
+}
+updateCountInCart()
 for (var i = 0; i < BEAN_PRODUCTS.length; i++) {
     var products1 = document.getElementById("products-list-id");
     products1.innerHTML = products1.innerHTML + "<div>"
@@ -17,23 +15,23 @@ for (var i = 0; i < BEAN_PRODUCTS.length; i++) {
         + "<div>" + BEAN_PRODUCTS[i].price + "</div>"
         + "<button onClick='addToCart(" + BEAN_PRODUCTS[i].id + ")' class='button'>В корзину</button>"
         + "</div>"
-
 }
 
-// Потрібно зберігати ід кожного продукта в масив щоб потім їх можна було відобразити в корзині
 function addToCart(id) {
-    //console.log(id);
-    
-    if (arrayCart[id] != undefined) {
-        arrayCart[id]++;
+    if (cartStorage[id] != undefined) {
+        cartStorage[id]++;
     }
     else {
-        arrayCart[id] = 1;
+        cartStorage[id] = 1;
     }
-    //console.log(arrayCart);
-  var number = document.getElementById("numberCart");
-  var count=Object.keys(arrayCart).length;
- number.innerHTML = count ;
- //console.log(number);
-   
+    updateCountInCart()
+    console.log(cartStorage)
+    localStorage.setItem('cartstorage', JSON.stringify(cartStorage));
+}
+function updateCountInCart() {
+    var number = document.getElementById("numberCart");
+    var count = Object.keys(cartStorage).length;
+    number.innerHTML = count;
+
+
 }

504e8ab5de46c12a89f465487c287e76888858f9
Dasha Dodnar <dawa.bodnar@gmail.com>
2021-04-13 20:16:55 +0300

    finished cart

--- cart/cart.js
+++ cart/cart.js
@@ -10,35 +10,31 @@ function send() {
     object.surname = surnameCart.value;
     object.contact = contactCart.value;
     object.info = infoCart.value;
-    // object.information = " Ім'я: " + nameCart.value + " Прізвище: " + surnameCart.value + " Номер: " + contactCart.value + " Дані відправки: " + infoCart.value;
     console.log(object)
-    var sent = document.getElementById("send1");
-    sent.innerHTML = "Ваше замовлення прийнято. Через декілька хвилин з вами звяжеться наш працівник. Дякуємо Вам"
+    var sentOrder = document.getElementById("sendOrder");
+    sentOrder.innerHTML = "Ваше замовлення прийнято. Через декілька хвилин з вами звяжеться наш працівник. Дякуємо Вам"
 }
 
-function emptyCart() { // якщо корзина пустий то відображати слово пусто а якщо ні то наповнювати корзину списком
+function emptyCart() {
     let empty = document.getElementById("products-table")
     let saveInCart = localStorage.getItem('cartstorage')
     if (saveInCart === null) {
         empty.innerHTML = "Пусто"
     } else {
         let cartStorage = JSON.parse(saveInCart);
-        console.log(Object.keys(cartStorage));
-        console.log(cartStorage)
-        console.log(saveInCart)
-        for (var i = 0; i < BEAN_PRODUCTS.length; i++) {
-            var mas = Object.keys(cartStorage);
-            var num = mas[i];
+        let arrayId = Object.keys(cartStorage);
+        for (var i = 0; i < arrayId.length; i++) {
+            var numberId = arrayId[i];
             var productsTable = document.getElementById("products-table");
             productsTable.innerHTML = productsTable.innerHTML +
-                "<div class='Price'>" +
-                "<div class='grid-name'>" +
-                "<img src='" + BEAN_PRODUCTS[num].bigPhotoUrl + "' id='foto1' class='foto-cart'>" +
-                "<div id='idBrand' class='text-name'>" + BEAN_PRODUCTS[num].name + "</div>" + "</div>" +
-                "<div>" + BEAN_PRODUCTS[num].price + "</div>" +
-                "<div>" + cartStorage[num] + "</div>" +
-                "<div>" + 200 + "</div>";
-            // console.log(num)
+                "<div class='products-coffee-cart'>" +
+                "<div class='info-coffee-cart'>" +
+                "<img src='" + "../" + BEAN_PRODUCTS[numberId].bigPhotoUrl + "'  class='foto-cart'>" +
+                "<div>" + BEAN_PRODUCTS[numberId].name + "</div>" + "</div>" +
+                "<div>" + BEAN_PRODUCTS[numberId].price + "</div>" +
+                "<div>" + cartStorage[numberId] + "</div>" +
+                "<div>" + 200 + "</div>" +
+                "</div>";
         }
 
     }
